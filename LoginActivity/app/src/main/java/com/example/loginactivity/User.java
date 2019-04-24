3a7a93da088db740bf89d3bebb87cd0c187fa993
Joseph <dacalculator@gmail.com>
2019-04-24 14:50:23 +1000

    Added charity and tutor classes, and added some fields to the user class (changed no existing code)

--- LoginActivity/app/src/main/java/com/example/loginactivity/User.java
+++ LoginActivity/app/src/main/java/com/example/loginactivity/User.java
@@ -2,8 +2,9 @@ package com.example.loginactivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import java.util.LinkedList;
 
-public class User {
+public abstract class User {
     private String userId;
     private String firstName;
     private String lastName;
@@ -11,6 +12,10 @@ public class User {
     private String password;
     private String userType;
     private String charityOrg;
+    private String rating;
+    private LinkedList<Note> uploadHistory;
+    private LinkedList<Event> eventHistory;
+    private LinkedList<Event> eventAttending;
 
 
     public User (String userId, String firstName, String lastName, String emailAddress, String password, String userType, String charityOrg) {
@@ -21,6 +26,9 @@ public class User {
         this.password = password;
         this.userType = userType;
         this.charityOrg = charityOrg;
+        uploadHistory = new LinkedList<Note>();
+        eventHistory = new LinkedList<Event>();
+        eventAttending = new LinkedList<Event>();
     }
 
     public String getUserId() {
@@ -50,4 +58,12 @@ public class User {
     public String getCharityOrg() {
         return charityOrg;
     }
+
+    public String getRating() { return rating; }
+
+    public LinkedList<Note> getUploadHistory() { return uploadHistory; }
+
+    public LinkedList<Event> getEventHistory() { return eventHistory; }
+
+    public LinkedList<Event> getEventAttending() { return eventAttending; }
 }

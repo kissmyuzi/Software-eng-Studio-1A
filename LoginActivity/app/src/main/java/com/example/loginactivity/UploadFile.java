46fb327932f8e087a5ef09bf16bad10b0a5f0713
Emanuel <emanuel.kindaro@student.uts.edu.au>
2019-04-26 17:38:28 +1000

    UploadFile Class is fully complete. I also added a permission within the AndroidManifest.xml file.

--- LoginActivity/app/src/main/java/com/example/loginactivity/UploadFile.java
+++ LoginActivity/app/src/main/java/com/example/loginactivity/UploadFile.java
@@ -5,7 +5,9 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +21,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,13 +30,18 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class UploadFile extends AppCompatActivity {
     Button selectFile, uploadFile;
     TextView notificationFile;
+    Uri pdfUri;
 
     FirebaseStorage storage;
     FirebaseDatabase database;
+    ProgressDialog progressDialog;
 
     @Override
 
@@ -51,14 +60,71 @@ public class UploadFile extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if(ContextCompat.checkSelfPermission(UploadFile.this,
+                if (ContextCompat.checkSelfPermission(UploadFile.this,
                         Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
 
                     selectPdf();
-                }
-                else
+                } else {
                     ActivityCompat.requestPermissions(UploadFile.this,
                             new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 9);
+                }
+            }
+        });
+
+        uploadFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (pdfUri != null) {
+                    uploadFile(pdfUri);
+                }
+                else {
+                    Toast.makeText(UploadFile.this, "Select a file.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void uploadFile(Uri pdfUri) {
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setTitle("We are uploading your file...");
+        progressDialog.setProgress(0);
+        progressDialog.show();
+
+        final String fileName = System.currentTimeMillis()+"";
+        StorageReference storageReference = storage.getReference();
+
+        storageReference.child("Uploads").child(fileName).putFile(pdfUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        String url = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                        DatabaseReference reference = database.getReference();
+                        reference.child(fileName).setValue(url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful())
+                                    Toast.makeText(UploadFile.this, "File has been successfully uploaded.", Toast.LENGTH_SHORT).show();
+                                else
+                                    Toast.makeText(UploadFile.this, "File has failed to upload.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(UploadFile.this, "File has failed to upload.", Toast.LENGTH_SHORT).show();
+            }
+
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+                int currentProgress = (int) (100*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                progressDialog.setProgress(currentProgress);
             }
         });
     }
@@ -67,9 +133,9 @@ public class UploadFile extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == 9 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             selectPdf();
+        } else {
+            Toast.makeText(UploadFile.this, "Please provide permission.", Toast.LENGTH_SHORT).show();
         }
-        else
-            Toast.makeText(UploadFile.this,"Please provide permission.", Toast.LENGTH_SHORT).show();
     }
 
     private void selectPdf() {
@@ -79,5 +145,16 @@ public class UploadFile extends AppCompatActivity {
         startActivityForResult(intent, 86);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == 86 && resultCode == RESULT_OK && data != null) {
+            pdfUri = data.getData();
+            notificationFile.setText("A file is selected: " + data.getData().getLastPathSegment());
+        }
+        else {
+            Toast.makeText(UploadFile.this,"Please select a file.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
 

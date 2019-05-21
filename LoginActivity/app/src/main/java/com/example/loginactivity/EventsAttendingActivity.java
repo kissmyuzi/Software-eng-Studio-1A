3942e5b2846dda989d0c29b4fbb936376934eb92
Hector <48663306+HdroguettA@users.noreply.github.com>
2019-05-21 22:42:39 +1000

    Made the activity look cool

--- LoginActivity/app/src/main/java/com/example/loginactivity/EventsAttendingActivity.java
+++ LoginActivity/app/src/main/java/com/example/loginactivity/EventsAttendingActivity.java
@@ -3,11 +3,17 @@ package com.example.loginactivity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.card.MaterialCardView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.Checkable;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -40,6 +46,10 @@ public class EventsAttendingActivity extends AppCompatActivity implements View.O
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        setCardViewBackgroundToRecyclerView(recyclerView);
+
         popList(events); //garbage until user data can be received
         btnAddEvent = findViewById(R.id.btnAddEvent);
         btnAddEvent.setOnClickListener(this);
@@ -60,5 +70,22 @@ public class EventsAttendingActivity extends AppCompatActivity implements View.O
 
     public void homeBtn(View view) {
         startActivity(new Intent(getApplicationContext(),StudentDashboardActivity.class));
+    }
+
+    private void setCardViewBackgroundToRecyclerView(RecyclerView recyclerView) {
+        MaterialCardView cardView = new MaterialCardView(this);
+        cardView.setCardBackgroundColor(ContextCompat.getColor(this, R.color.colorWhite));
+        cardView.setUseCompatPadding(true);
+        cardView.setPreventCornerOverlap(false);
+        cardView.setRadius(65);
+
+        int cardShadow = (int) cardView.getCardElevation();
+        recyclerView.setPadding(cardView.getContentPaddingLeft() + cardShadow,
+                cardView.getContentPaddingTop() + cardShadow + 3,
+                cardView.getContentPaddingRight() + cardShadow,
+                cardView.getContentPaddingBottom() + cardShadow + 3);
+        ViewCompat.setElevation(recyclerView, 8);
+        recyclerView.setBackground(cardView.getBackground());
+
     }
 }
